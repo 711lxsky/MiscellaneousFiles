c3e0f4a6535f1c063adaa6d70e9a4a80e5771516
711lxsky <yuyangcoding@gmail.com>
2024-03-25 10:12:04 +0000

    update

--- DailyGo/LeetCode_Hot100/T394_240324.go
+++ DailyGo/LeetCode_Hot100/T394_240324.go
@@ -13,19 +13,27 @@ func decodeString(s string) string {
 	num := 0
 	for i < n {
 		if s[i] >= '0' && s[i] <= '9' {
-			if len(str) != 0 {
-				strStack = append(strStack, string(str))
-				str = []byte{}
-			}
 			num *= 10
 			num += int(s[i] - '0')
+		} else if s[i] >= 'a' && s[i] <= 'z' {
+			str = append(str, s[i])
 		} else if s[i] == '[' {
 			numStack = append(numStack, num)
 			num = 0
-		} else if s[i] >= 'a' && s[i] <= 'z' {
-			str = append(str, s[i])
+			strStack = append(strStack, string(str))
+			str = []byte{}
+		} else { // 这时候只有]
+			tmp := []byte{}
+			repeatTime := numStack[len(numStack)-1]
+			// 没有除掉重复次数
+			numStack = numStack[:len(numStack)-1]
+			for j := 0; j < repeatTime; j++ {
+				tmp = append(tmp, str...)
+			}
+			str = append([]byte(strStack[len(strStack)-1]), tmp...)
+			strStack = strStack[:len(strStack)-1]
 		}
-
 		i++
 	}
+	return string(str)
 }
