5e5306cfda1d0f2089f32c7c77f7f677b7a0195b
711lxsky <yuyangcoding@gmail.com>
2024-05-07 11:16:12 +0800

    update leetcode solving problem code for python and Go

---
+++ DailyGo/LeetCode_Hot100/T416_240507.go
@@ -0,0 +1,20 @@
+package LeetCode_Hot100
+
+func canPartition(nums []int) bool {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	if sum%2 != 0 {
+		return false
+	}
+	target := sum / 2
+	dp := make([]bool, target+1)
+	dp[0] = true
+	for _, num := range nums {
+		for i := target; i >= num; i-- {
+			dp[i] = dp[i] || dp[i-num]
+		}
+	}
+	return dp[target]
+}
