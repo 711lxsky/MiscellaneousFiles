9544ad36dccaa9412d4fc7f698fe75c414970617
YuleYoungZoe <yuyangcoding@gmail.com>
2024-07-27 00:16:37 +0800

    -add
    update leetcode problem-list classic150 solving code with Java

--- DailyJava/LeetcodeUse/src/LeetCodeClassic150/T139_240725.java
+++ DailyJava/LeetcodeUse/src/LeetCodeClassic150/T139_240725.java
@@ -13,12 +13,11 @@ public class T139_240725 {
                 record.put(str, true);
             }
             int n = s.length();
-            boolean[] dp = new boolean[n+1];
+            boolean[] dp = new boolean[n + 1];
             dp[0] = true;
             for(int i = 1; i <= n; i ++){
                 for(int j = 0; j < i; j ++){
-                    String cur = s.substring(j, i);
-                    if(dp[j] && record.containsKey(cur)){
+                    if(dp[j] && record.containsKey(s.substring(j, i))){
                         dp[i] = true;
                         break;
                     }
